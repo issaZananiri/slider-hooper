6c49a365ef4a9f9ade01aab6912768fb653475ab
Issa zananiri <issa_zananiri@hotmail.com>
2021-09-08 16:33:20 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+  
+    <Card v-bind:product="product" @change-img="changeImg"/>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card";
+
+
+
+
+export default {
+  name: "App",
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    Card
+  },
+  methods:{
+    changeImg(path){
+      this.product.mainImg = path;
+    }
+  }
+};
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 100vw;
+  min-height: 100vh;
+  background: #E5E5E5;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,160 @@
+<template>
+<div class="card-wrapper">
+  <div class='card1'>
+
+
+      <div class="card-header1">חבילות אינטרנט</div>
+
+    
+      <span class="card-span">המהירים- ואתם נהנים מחויית גלישה</span>
+       
+      <span class="card-span">מצטרפת למהפכת הסיבים </span>
+ 
+      <span class="card-span">עוצמתית וזורמתית מתמיד</span>
+
+<div>
+  <hooper class="slideA"  ltr="true" group="group1" :itemsToShow="5"  :progress="true" :autoPlay="true" :playSpeed="3000" :centerMode="true">
+    
+  <slide>
+    slide 1
+    <span>this is a test</span>
+  </slide>
+  <slide>
+    slide 2 t
+  </slide>
+  <slide>
+    slide 3
+  </slide>
+  <slide>
+    slide 4
+  </slide>
+  <slide>
+    slide 5
+  </slide>
+  <slide>
+    slide 6
+  </slide>
+
+  <hooper-navigation slot="hooper-addons"></hooper-navigation>
+  <hooper-pagination slot="hooper-addons"></hooper-pagination>
+</hooper>
+  <hooper  class="slideB" style="width:728px; padding-left: 82px;" group="group1" :centerMode="true" :itemsToShow="1.36"  >
+  <slide  style="padding-right:45px;">
+    slide 1
+    
+  </slide>
+  <slide style="padding-right:25px;">
+    slide 2
+  </slide >
+  <slide style=" all: none;">
+    slide 3 
+    <i class="bi bi-check"></i>
+    <span>hi</span>
+  </slide>
+  <slide style=" all: none;">
+    slide 4
+  </slide>
+  <slide style=" all: none;">
+    slide 5
+  </slide>
+  <slide style=" all: none;">
+    slide 6
+  </slide>
+</hooper>
+
+</div>
+</div>
+  </div>
+</template>
+
+<script>
+
+import { Hooper, Slide,  Navigation as HooperPagination} from 'hooper';
+import 'hooper/dist/hooper.css';
+
+import '../style.css'
+
+
+
+
+
+export default {
+  components: {
+    HooperPagination,
+    Hooper,
+    Slide
+  },
+ 
+  watch: {
+    carouselData () {
+      this.$refs.carousel.slideTo(this.carouselData);
+    }
+  },
+  methods:{
+    carouselButton(index){
+      this.$refs.carousel.slideTo(index);
+      this.activeLinkIndex = index
+    },
+
+
+    scrollTop(){
+      this.$refs.carousel.slidePrev();
+    },
+    scrollBottom(){
+      this.$refs.carousel.slideNext();
+    },
+
+    updateCarousel(payload) {
+      this.myCarouselData = payload.currentSlide;
+    },
+
+  },
+  data(){
+    return{
+      carouselData: 0,
+      quantity: 1,
+    
+      activeLinkIndex: null,
+  
+      hooperSettings: {
+              itemsToShow: 1,
+              centerMode: true,
+              infiniteScroll: true ,
+              breakpoints: {
+                1240:{
+                  itemsToShow: 1,
+                  vertical: true,
+                },
+                992: {
+                  itemsToShow: 1,
+                  vertical: true,
+                },
+                768:{
+                  itemsToShow: 1,
+                  vertical:true,
+                },
+                576:{
+                 itemsToShow: 1,
+                  vertical: true,
+                },
+                360:{
+               itemsToShow: 1,
+                  vertical: true,
+                }
+              }
+            },
+      mainHooperSettings:{
+        infiniteScroll: false,
+        centerMode: true,
+        itemsToShow:1
+      }
+    }
+  },
+  mounted() {
+    
+    setTimeout(() => this.$refs.mainCarousel.updateWidth().bind(this), 50)
+    
+  },
+};
+
+</script>
